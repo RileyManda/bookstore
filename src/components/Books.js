4acca53f1aee5bef2a9d4f2669713fd190f5fd6a
RileyManda <rileymanda69@gmail.com>
2023-07-27 16:51:34 +0200

    Add new book font face

--- src/components/Books.js
+++ src/components/Books.js
@@ -7,6 +7,7 @@ const Books = () => (
   <div className="books flex-container flex-column">
     <Book />
     <div className="hl" />
+    <p className="add-new-book-text font-face-montserrat">ADD NEW BOOK</p>
     <BookForm />
   </div>
 );
